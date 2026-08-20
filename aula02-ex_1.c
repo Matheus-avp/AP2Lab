3671f5d81e5a0d943f1d3f69d01105dd63a8b84c
Matheus-avp <matheusaveiro.pereira@gmail.com>
2026-08-20 22:54:36 +0000

    ex-01

--- aula02-ex_1.c
+++ aula02-ex_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+    int a,b,soma,subtracao,multiplicacao,divisao; 
+    printf("Coloque dois numeros: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    soma = a + b;
+    subtracao = a - b;
+    multiplicacao = a * b;
+    divisao = a / b;
+    printf("Soma: %d \n", soma);
+    printf("Subtracao : %d \n", subtracao);
+    printf("Mulitplicacao: %d \n", multiplicacao);
+    printf("Divisao: %d \n", divisao);
+    return 0;
+}
